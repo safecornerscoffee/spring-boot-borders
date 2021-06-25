beb990d716b4aafc9efeb2b397606ffb516638f8
safecorners <safeconers.mobile@gmail.com>
2021-06-25 23:09:54 +0900

    spring initializer

---
+++ src/main/java/com/safecornerscoffee/borders/BordersApplication.java
@@ -0,0 +1,13 @@
+package com.safecornerscoffee.borders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BordersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BordersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/safecornerscoffee/borders/BordersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safecornerscoffee.borders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BordersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
