f6ddc8342793773739812b12a7c76c17516192ff
safecorners <safeconers.mobile@gmail.com>
2021-07-07 02:49:01 +0900

    [#44] disabled InMemoryWebSecurityConfig

--- src/main/java/com/safecornerscoffee/borders/security/InMemoryWebSecurityConfig.java
+++ src/main/java/com/safecornerscoffee/borders/security/InMemoryWebSecurityConfig.java
@@ -7,8 +7,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
-@Configuration
-@EnableWebSecurity
+//@Configuration
+//@EnableWebSecurity
 public class InMemoryWebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
