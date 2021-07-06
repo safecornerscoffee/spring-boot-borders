80f81d05aa0cd96d11550d7a25ad96b554cd5535
safecorners <safeconers.mobile@gmail.com>
2021-07-07 00:49:23 +0900

    #43 import RequestPostProcessors

--- src/test/java/com/safecornerscoffee/borders/integration/controller/OrderControllerIntegrationTest.java
+++ src/test/java/com/safecornerscoffee/borders/integration/controller/OrderControllerIntegrationTest.java
@@ -28,6 +28,8 @@ import java.util.List;
 import java.util.Map;
 
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
