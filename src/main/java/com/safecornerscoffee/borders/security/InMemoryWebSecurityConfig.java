2932556874c7a8cf7486a9ec4685cf5459a39565
safecorners <safeconers.mobile@gmail.com>
2021-07-08 13:07:08 +0900

    [#44] fix *.sql indent style

--- src/main/java/com/safecornerscoffee/borders/security/InMemoryWebSecurityConfig.java
+++ src/main/java/com/safecornerscoffee/borders/security/InMemoryWebSecurityConfig.java
@@ -1,14 +1,16 @@
 package com.safecornerscoffee.borders.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
-//@Configuration
-//@EnableWebSecurity
+@Profile("inMemoryAuthentication")
+@Configuration
+@EnableWebSecurity
 public class InMemoryWebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
