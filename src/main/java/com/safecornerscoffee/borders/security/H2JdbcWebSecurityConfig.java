8ce273f16db85382fecf5e3f788284207313b29c
safecorners <safeconers.mobile@gmail.com>
2021-07-08 18:25:02 +0900

    [#44] Use BCryptPasswordEncoder

--- src/main/java/com/safecornerscoffee/borders/security/H2JdbcWebSecurityConfig.java
+++ src/main/java/com/safecornerscoffee/borders/security/H2JdbcWebSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
@@ -51,6 +52,6 @@ public class H2JdbcWebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     public PasswordEncoder encoder() {
-        return new NoOpPasswordEncoder();
+        return new BCryptPasswordEncoder();
     }
 }
